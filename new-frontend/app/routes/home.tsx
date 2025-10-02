7242c5ea3730a9f94057fad0c7677e8097fb76ed
Hubert Leo <hubertleo@Huberts-MacBook-Air.local>
2025-10-02 17:25:10 -0400

    fixed type issues

--- new-frontend/app/routes/home.tsx
+++ new-frontend/app/routes/home.tsx
@@ -1,61 +1,101 @@
 import type { Route } from "./+types/home";
-import { useEffect } from "react";
+import { useEffect, useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
+function Icon({ text }: { text: string }) {
+  return (
+    <div className="w-15 h-15 border border-black rounded-xl flex items-center justify-center text-center cursor-pointer">
+      {text}
+    </div>
+  );
+}
 
-function Icon({text } : {text: string}) {
-  return(<div className="w-15 h-15 border border-black rounded-xl flex items-center justify-center text-center cursor-pointer">
-    {text}
-  </div>)
+function DragNDrop({file, setFile, filetype} : {file: File | null, setFile: React.Dispatch<React.SetStateAction<File| null>>, filetype: string}) {
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFile(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="border border-black">
+      <input {...getInputProps()} />
+      {!file?.name ? <p>Drag 'n' drop {filetype}</p> : <p>{file?.name}</p>}
+    </div>
+  );
 }
 
 function DataSourceBlock() {
-  return(<div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
-    Data Source
-    </div>)
+  const [xptFile, setxptFile] = useState<File | null>(null);
+  const [pdfFile, setpdfFile] = useState<File | null>(null);
+
+  const uploadFiles = () => {
+    console.log(xptFile?.name, pdfFile?.name);
+  }
+
+  return (
+    <div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex flex-col items-center justify-center">
+      Data Source
+      <DragNDrop file={xptFile} setFile={setxptFile} filetype=".xpt"/>
+      <DragNDrop file={pdfFile} setFile={setpdfFile} filetype=".pdf"/>
+      <button className="bg-blue-300" onClick={() => {uploadFiles()}}>Upload</button>
+    </div>
+  );
 }
 
 function FindBlock() {
-  return(<div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
-    Find Block
-    </div>)
+  return (
+    <div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
+      Find Block
+    </div>
+  );
 }
 
 function ExtractInfoBlock() {
-  return(<div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
-    Extract Info
-    </div>)
+  return (
+    <div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
+      Extract Info
+    </div>
+  );
 }
 
 function SelectBlock() {
-  return(<div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
-    Select
-    </div>)
+  return (
+    <div className="w-60 h-30 top-20 bg-white rounded-xl border border-black text-center flex items-center justify-center">
+      Select
+    </div>
+  );
 }
-
-
 
 function SideBar() {
- return(<div className="absolute left-2 top-1/2 -translate-y-1/2 h-100 w-20 bg-white rounded-xl border border-black  gap-3 py-10 shadow-xl flex flex-col items-center text-sm">
-  <Icon text = "Data Source"></Icon>
-  <Icon text = "Get Columns"></Icon>
-  <Icon text = "Extract Info"></Icon>
-  <Icon text = "Select"></Icon>
-</div>
-)
+  return (
+    <div className="absolute left-2 top-1/2 -translate-y-1/2 h-100 w-20 bg-white rounded-xl border border-black  gap-3 py-10 shadow-xl flex flex-col items-center text-sm">
+      <Icon text="Data Source"></Icon>
+      <Icon text="Get Columns"></Icon>
+      <Icon text="Extract Info"></Icon>
+      <Icon text="Select"></Icon>
+    </div>
+  );
 }
 
-
 export default function Home() {
-  return (<div className="w-full min-h-screen" 
-  style = {{backgroundImage : "radial-gradient(circle, rgba(0,0,0,0.2) 1.5px, transparent 1px)", backgroundSize: "20px 20px"}}
-  > 
-  <div className = "flex gap-5">
-  <DataSourceBlock/>
-  <FindBlock/>
-  <ExtractInfoBlock/>
-  <SelectBlock/>
-  </div>
-    <SideBar/>
-    
-  </div>);
+  return (
+    <div
+      className="w-full min-h-screen"
+      style={{
+        backgroundImage:
+          "radial-gradient(circle, rgba(0,0,0,0.2) 1.5px, transparent 1px)",
+        backgroundSize: "20px 20px",
+      }}
+    >
+      <div className="flex gap-5">
+        <DataSourceBlock />
+        <FindBlock />
+        <ExtractInfoBlock />
+        <SelectBlock />
+      </div>
+      <SideBar />
+    </div>
+  );
 }
